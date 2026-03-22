59b1bfb16e6772f62d1ceb1587703f0f61888e61
Salvatore <pepesalvatore12@gmail.com>
2026-03-22 19:34:00 +0100

    add unique URL for all the fethces

---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+//uso questa cartella per pulire tutte le rotte e le chiamate fetch, salvando l'url in un unica variabile
+
+const BASE_URL = "https://e-commerce-backend-c9cn.onrender.com/product"
+
+export const API_ENDPOINT = {
+  //prodotti
+  PRODUCTS: `${BASE_URL}/product`,
+
+  //categorie
+  CATEGORIES: `${BASE_URL}/categories`,
+
+  //autenticazioni
+  LOGIN: `${BASE_URL}/auth/login`,
+  REGISTER: `${BASE_URL}/auth/register`,
+
+  //ordini
+  ORDERS: `${BASE_URL}/orders`,
+  MY_ORDERS: `${BASE_URL}/orders/me`,
+}
